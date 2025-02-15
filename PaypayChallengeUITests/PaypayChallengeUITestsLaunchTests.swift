ac4a8fd9373afd108164a9aff71d7c6d56a89a96
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2025-02-15 22:25:56 +0800

    Initial Commit

---
+++ PaypayChallengeUITests/PaypayChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PaypayChallengeUITestsLaunchTests.swift
+//  PaypayChallengeUITests
+//
+//  Created by Chung Han Hsin on 2025/2/15.
+//
+
+import XCTest
+
+final class PaypayChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
